a1a49a753864ca5d757194a8c4e2b7ad5ec80c7d
Ramiza1998 <ramizayakubu994@gmail.com>
2022-04-06 08:05:19 -0700

    first commit

---
+++ results.js
@@ -0,0 +1,18 @@
+const  results=[
+    {name:"Winy Dari", class: "Form One", marks:8},
+    {name:"Zakaria Ab", class: "Form One", marks:6},
+    {name:"Flex Dass", class: "Form One", marks:2},
+    {name:"Awudu Dery", class: "Form One", marks:10},
+    {name:"Dady Ama", class: "Form One", marks:7},
+    {name:"Ibrahim Amatu", class: "Form One", marks:3},
+    {name:"Yakubu ramiza", class: "Form One", marks:1},
+    {name:"Rafiq Hanat", class: "Form One", marks:4},
+    {name:"Abu Amina", class: "Form One", marks:5},
+    {name:"Issah Daud", class: "Form One", marks:9},
+    
+
+
+]
+
+
+module.exports=results
